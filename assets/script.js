45e505c8950ed4d50f4e811973745055e57d06a2
Fernando Guillen <fguillen.mail@gmail.com>
2023-01-10 15:58:54 +0100

    Starting with recommendations

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,54 @@
+const ANIMATION_TIME = 3000
+const ANIMATION_WAIT_MIN = 2000
+const ANIMATION_WAIT_MAX = 10000
 
-
-function changeImage(element, url) {
-    element.setAttribute("src", url);
+function animationPlay(element) {
+    console.log("animationPlay");
+    if (element.getAttribute("src") != element.getAttribute("data-animated-version")) {
+        console.log("animationPlay Really: " + element.getAttribute("data-animated-version"));
+        element.setAttribute("src", element.getAttribute("data-animated-version"));
+    }
 }
+
+function animationPause(element) {
+    console.log("animationPause");
+    if (element.getAttribute("src") != element.getAttribute("data-static-version")) {
+        element.setAttribute("src", element.getAttribute("data-static-version"));
+    }
+}
+
+function animateForAWhile(element, time = ANIMATION_TIME) {
+    if (element.getAttribute("src") != element.getAttribute("data-animated-version")) {
+        animationPlay(element);
+
+        setTimeout(() => {
+            animationPause(element);
+        }, time);
+    }
+}
+
+function animateRandomly(element) {
+    var waitTime = Math.random() * (ANIMATION_WAIT_MAX - ANIMATION_WAIT_MIN) + ANIMATION_WAIT_MIN;
+
+    setTimeout(() => {
+        animateForAWhile(element);
+        animateRandomly(element);
+    }, waitTime);
+}
+
+function activateAnimations() {
+    var elements = document.getElementsByClassName("animated-image");
+
+    for (const element of elements) {
+        element.addEventListener("mouseover", (event) => {
+            console.log("mouseover");
+            animateForAWhile(element);
+        });
+        animateRandomly(element);
+    };
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    activateAnimations();
+});
